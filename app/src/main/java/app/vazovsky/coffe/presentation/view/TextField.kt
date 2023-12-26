acae47207d5ae0397e337ba580d4da1662383535
m.klyueva <vazovsky.app@gmail.com>
2023-12-27 00:04:40 +0400

    Fix text field

--- app/src/main/java/app/vazovsky/coffe/presentation/view/TextField.kt
+++ app/src/main/java/app/vazovsky/coffe/presentation/view/TextField.kt
@@ -1,23 +1,25 @@
 package app.vazovsky.coffe.presentation.view
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
-import app.vazovsky.coffe.R
+import androidx.compose.ui.unit.sp
 import app.vazovsky.coffe.extensions.orDefault
 import app.vazovsky.coffe.presentation.ui.theme.Champagne
 import app.vazovsky.coffe.presentation.ui.theme.CoyoteBrown
@@ -43,28 +45,15 @@ fun AppTextField(
         )
     }
     Space(8.dp)
-    OutlinedTextField(
+
+    BasicTextField(
         modifier = modifier
             .fillMaxWidth()
-            .requiredHeight(48.dp),
+            .requiredHeight(48.dp)
+            .border(2.dp, CoyoteBrown, RoundedCornerShape(24.dp)),
         value = value,
         onValueChange = onValueChange,
-        placeholder = {
-            Text(
-                text = placeholder.orDefault(),
-                style = MaterialTheme.typography.bodyLarge,
-                color = Champagne,
-            )
-        },
-        shape = RoundedCornerShape(24.dp),
-        colors = OutlinedTextFieldDefaults.colors(
-            focusedBorderColor = CoyoteBrown,
-            unfocusedBorderColor = CoyoteBrown,
-            focusedTextColor = PaleTaupe,
-            unfocusedTextColor = PaleTaupe,
-        ),
         singleLine = true,
-        //overflow = TextOverflow.Visible,
         maxLines = 1,
         visualTransformation = visualTransformation,
         keyboardOptions = KeyboardOptions(
@@ -74,6 +63,21 @@ fun AppTextField(
         keyboardActions = KeyboardActions(
             onDone = onDoneClick,
         ),
-        textStyle = MaterialTheme.typography.bodyLarge,
-    )
+        textStyle = MaterialTheme.typography.titleSmall.copy(fontSize = 18.sp, color = PaleTaupe),
+    ) { innerTextField ->
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(PaddingValues(18.dp, 13.dp))
+        ) {
+            if (value.isBlank()) {
+                Text(
+                    text = placeholder.orDefault(),
+                    color = Champagne,
+                    style = MaterialTheme.typography.titleSmall.copy(fontSize = 18.sp),
+                )
+            }
+            innerTextField()
+        }
+    }
 }
